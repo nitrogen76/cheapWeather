a57264d112331f439f0314a2cadd19135d8b1848
L Green <leo@unixjerk.org>
2020-09-05 00:36:30 -0500

    c

--- scripts/influx-wunderground.py
+++ scripts/influx-wunderground.py
@@ -37,7 +37,7 @@ client = InfluxDBClient(host=(influxHost), port=8086, username=(influxUser), pas
 tempF=client.query(query + station)
 tempResult = tempF.raw
 pprint(tempResult)
-print(tempResult)["series:values"]
+print(tempResult)["series"]
 #results.raw
 #humidity=client.query('SELECT last("humidity")   FROM (thermometer)')
 #windspeed=client.query('SELECT last("wind_avg_km_h") * 0.6213712 FROM (station)')
