29e3a4559e2bb4f767645b1c6e4c6d2dcec7c79c
Leo Green <leo@unixjerk.org>
2024-01-14 15:39:36 -0600

    Did some refactoring.  used metpy, and used pint to keep values sorted.

--- scripts/influx-wunderground.py
+++ scripts/influx-wunderground.py
@@ -6,24 +6,38 @@
 
 ## Imports
 import configparser
+import argparse
 import requests
 import re
-import weathermath
-import heatindex
 import metpy.calc as mpcalc
 from metpy.units import units
-
 from influxdb import InfluxDBClient
 
+## Argumenmts
+parser = argparse.ArgumentParser('description: generate some values every minute from values in influx, and also send data to wunderground.')
+parser.add_argument('-d','--debug',action='store_true', help='Show debug information')
+parser.add_argument('-n','--no_wunderground',action='store_false',help='Do not use wunderground')
+args=parser.parse_args()
+DEBUG=(args.debug)
+useWunder=(args.no_wunderground)
+
+if DEBUG==True:
+    print ("DEBUG: ENABLED")
 
 # Get config info
 config = configparser.ConfigParser()
 config.read('/local/leo/cheapWeather/cheapWeather.ini')
 
 ### Variables
-softwareVersion='&softwaretype=cheapWeather%20version%20Zonda'
-wundergroundUser=config.get('Wunderground','user')
-wundergroundPass=config.get('Wunderground','password')
+softwareVersion='&softwaretype=cheapWeather%20version%20swullock'
+
+## If we aren't using wunderground, we don't need to get the variables
+if useWunder==True:
+    wundergroundUser=config.get('Wunderground','user')
+    wundergroundPass=config.get('Wunderground','password')
+if useWunder==False and DEBUG==True:
+    print ("DEBUG: Wunderground disabled, variables not read")
+        
 influxUser=config.get('Influx','user')
 influxPass=config.get('Influx','password')
 influxDB=config.get('Influx','database')
@@ -33,28 +47,22 @@ station=config.get('Query','station')
 windStation=config.get('Query','windstation')
 baroStation=config.get('Query','barostation')
 
-tempQuery='SELECT last("temperature_C") *1.8+32   FROM '
 tempQueryC='SELECT last("temperature_C") FROM '
 humidityQuery='SELECT last("humidity")   FROM '
-windspeedQuery='SELECT last("wind_avg_km_h") * 0.6213712 FROM '
-winddirQuery='SELECT last("wind_dir_deg")  FROM '
-windgustQuery='SELECT top("wind_avg_km_h", 1) * 0.6213712 FROM '
+windspeedQuery='SELECT last("wind_avg_km_h") FROM '
+winddirQuery='SELECT last("wind_dir_deg") FROM '
+windgustQuery='SELECT top("wind_avg_km_h", 1) FROM '
 gustTime='WHERE time > now() - 15m'
 baroQuery='SELECT last("Barometer") FROM '
 
-WUurl = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?"
-WUcreds = "ID=" + wundergroundUser + "&PASSWORD="+ wundergroundPass
+if useWunder==True:
+   WUurl = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?"
+   WUcreds = "ID=" + wundergroundUser + "&PASSWORD="+ wundergroundPass
+
 ######
 
 client = InfluxDBClient(host=(influxHost), port=8086, username=(influxUser), password=(influxPass), database=(influxDB))
 
-
-## Get last temp
-tmp=client.query(tempQuery + station)
-tmpString=str(tmp)
-tmpSliced=tmpString.split(':')[5]
-tempF=tmpSliced.split('}')[0]
-tempF=tempF.strip()
 
 # get last temp C
 tmpC=client.query(tempQueryC + station)
@@ -63,9 +71,7 @@ tmpSliced=tmpString.split(':')[5]
 tempC=tmpSliced.split('}')[0]
 tempC=tempC.strip()
 tempC=float(tempC)
-
-
-
+tempPint=(tempC * units.degC)
 
 ## Get last humidity
 tmp=client.query(humidityQuery + station)
@@ -74,13 +80,17 @@ tmpSliced=tmpString.split(':')[5]
 humidityP=tmpSliced.split('}')[0]
 humidityP=humidityP.strip()
 humidityPF=float(humidityP)
+##print ("humidityPercent= ",humidityPF)
+humidityPint=(humidityPF * units.percent)
 
 ## Get last windspeed
 tmp=client.query(windspeedQuery + windStation)
 tmpString=str(tmp)
 tmpSliced=tmpString.split(':')[5]
-windMPH=tmpSliced.split('}')[0]
-windMPH=windMPH.strip()
+windKPH=tmpSliced.split('}')[0]
+windKPH=windKPH.strip()
+windKPH=float(windKPH)
+windPint=(windKPH * units.kilometer_per_hour)
 
 ## Get last winddir
 tmp=client.query(winddirQuery + windStation)
@@ -88,13 +98,16 @@ tmpString=str(tmp)
 tmpSliced=tmpString.split(':')[5]
 windDIR=tmpSliced.split('}')[0]
 windDIR=windDIR.strip()
+windDIRPint=(windDIR * units.degrees)
 
 ## get last windgust
 tmp=client.query(windgustQuery + windStation + gustTime)
 tmpString=str(tmp)
 tmpSliced=tmpString.split(':')[5]
-windGustMPH=tmpSliced.split('}')[0]
-windGustMPH=windGustMPH.strip()
+windGustKPH=tmpSliced.split('}')[0]
+windGustKPH=windGustKPH.strip()
+windGustKPH=float(windGustKPH)
+windGustPint=(windGustKPH * units.kilometer_per_hour)
 
 ## get last barometer
 tmp=client.query(baroQuery + baroStation)
@@ -102,45 +115,45 @@ tmpString=str(tmp)
 tmpSliced=tmpString.split(':')[5]
 baroINHG=tmpSliced.split('}')[0]
 baroINHG=baroINHG.strip()
-BBB=float(baroINHG)
+baroINHG=float(baroINHG)
+baroPint=(baroINHG * units.inHg)
 
-#print("TempF" + tempF)
-#print("Humidity" + humidityP)
-##print ("Wind MPH "+ windMPH)
-#print("Direction " + windDIR)
-#print("Gust " + windGustMPH)
-#print("Barometer " + baroINHG)
+## Use metpy to calculate some values
+dewPointPint=mpcalc.dewpoint_from_relative_humidity(tempPint, humidityPint)
+wetBulbPint=mpcalc.wet_bulb_temperature(baroPint,tempPint,dewPointPint)
+windchillPint=mpcalc.windchill(tempPint,windPint,False,False)
+heatindexPint=mpcalc.heat_index(tempPint,humidityPint,False)
 
-dewPoint=weathermath.get_dew_point_c(tempC,humidityPF)
-heatIndex=heatindex.from_celsius(tempC,humidityPF)
-# Heat index doesn't do the right thing if the temp is too low
-## so if it's below 80º f, then there isn't any heat index...
-if tempC <= 26:
-    heatIndex=float(tempC)
+## If temp is below what makes sense for heatindex, just make it equal to temperature
+if tempPint.magnitude <= 25:
+   heatindexPint = tempPint
+   if DEBUG==True:
+       print ("DEBUG: temp value too low to generate a heat index")
+## if temp is above what makes sense for windchill, set it to the temp.
+if tempPint.magnitude >= 10:
+    windchillPint = tempPint
+    if DEBUG==True:
+        print ("DEBUG: temp value too high to generate a windchill")
 
-dewPointFlo=((dewPoint)* 1.8000 + 32.00)
-heatIndexFlo=((heatIndex)* 1.8000 + 32.00)
-heatIndexF=str(heatIndexFlo)
-dewPointF=str(dewPointFlo)
-
-## Wet Bulb Calculations
-T=(tempC * units.degC)
-B=(BBB * units.inHg)
-D=(dewPoint * units.degC)
-
-
-wbt_raw=mpcalc.wet_bulb_temperature(B,T,D)
+if DEBUG==True:
+   print("DEBUG: Values grabbed and generated")
+   print("     TempF" , (tempPint.to('degF').magnitude))
+   print("     Humidity" , humidityP)
+   print("     Wind MPH ", (windPint.to('mile_per_hour').magnitude))
+   print("     Direction " ,  windDIR)
+   print("     Gust " ,  (windGustPint.to('mile_per_hour').magnitude))
+   print("     Barometer " , baroINHG)
+   print("     heat index: ",(heatindexPint.to('degF').magnitude))
+   print("     Windchill: ", (windchillPint.to('degF').magnitude))
 
 
-
-wbt=float(str(wbt_raw).replace("degree_Celsius", ""))
 
 
 dewpointJSON = [{"measurement":"Dewpoint",
 
     "fields":
     {
-    "dewpoint":(dewPoint)
+    "dewpoint":(dewPointPint.magnitude)
     }
     },
     ]
@@ -149,7 +162,7 @@ heatIndexJSON = [{"measurement":"Heatindex",
 
     "fields":
     {
-    "heatindex":(heatIndex)
+    "heatindex":(heatindexPint.magnitude)
     }
     },
     ]
@@ -158,23 +171,43 @@ WBTIndexJSON = [{"measurement":"Wet Bulb Temp",
 
     "fields":
     {
-    "Wet Bulb Temp":(wbt)
+    "Wet Bulb Temp":(wetBulbPint.magnitude)
     }
     },
     ]
 
+windchillJSON = [{"measurement":"Windchill",
 
-##print (dewpointJSON)
-client.write_points(dewpointJSON)
-client.write_points(heatIndexJSON)
-client.write_points (WBTIndexJSON)
+    "fields":
+    {
+    "Windchill":(windchillPint.magnitude)
+    }
+    },
+    ]
+
+if DEBUG==False:
+    client.write_points(dewpointJSON)
+    client.write_points(heatIndexJSON)
+    client.write_points (WBTIndexJSON)
+    client.write_points (windchillJSON)
+
+if DEBUG==True:
+    print ("DEBUG: Not sending to INFLUX")
+    print ("     This is what would have been sent")
+    print ("     dewpoint JSON: ",dewpointJSON)
+    print ("     heatIndexJSON: ",heatIndexJSON)
+    print ("     Wet Bulb JSON: ",WBTIndexJSON)
+    print ("     Windchill JSON: ",windchillJSON)
 ## hack hack bad code alert
 
+if useWunder==True:
+           wundergroundRequest=(WUurl + WUcreds + "&dateutc=now&action=updateraw" + "&humidity=" + str(humidityP) + "&tempf=" + str(tempPint.to('degF').magnitude) + "&winddir=" + str(windDIR) + "&windspeedmph=" + str(windPint.to('mile_per_hour').magnitude) + "&windgustmph=" + str(windGustPint.to('mile_per_hour').magnitude) + "&baromin=" + str(baroINHG) + "&dewptf=" + str(dewPointPint.to('degF').magnitude) + softwareVersion)
+if useWunder==True and DEBUG==False:
+   httpstatus=requests.get(wundergroundRequest)
+   print(("Received " + str(httpstatus.status_code) + " " + str(httpstatus.text)))
+   if DEBUG==True and useWunder==True:
+      print ("DEBUG: wunderground URL Generated")
+      print (wundergroundRequest)
 
-wundergroundRequest=(WUurl + WUcreds + "&dateutc=now&action=updateraw" + "&humidity=" + humidityP + "&tempf=" + tempF + "&winddir=" + windDIR + "&windspeedmph=" + windMPH + "&windgustmph=" + windGustMPH + "&baromin=" + baroINHG + "&dewptf=" +dewPointF + softwareVersion)
-##print (wundergroundRequest)
-httpstatus=requests.get(wundergroundRequest)
-print(("Received " + str(httpstatus.status_code) + " " + str(httpstatus.text)))
-
-
-##print(dewPoint)
+if useWunder==False and DEBUG==True:
+   print ("DEBUG: Wunderground send disabled")
