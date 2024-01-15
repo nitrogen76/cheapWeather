9a7670079683ef2b641646c0b2a6e6e8497dc90b
Leo Green <leo@unixjerk.org>
2024-01-15 13:49:49 -0600

    weiorfyuoqwiuefg

--- scripts/433scoop.sh
+++ scripts/433scoop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ## This isn't really usable as a script, it's just here to show you commandline examples.
-rtl_433 -d:00000433 -M time:unix:usec:utc -R 142 -R 40 -R 73 -R 113 -R 42 -M protocol -M level -C si -F "influx://influxDB.ass.nurgle.net:8086/write?db=rtl433&p=2sLkJHH8Uh0Y36pTC&u=weather" &
+rtl_433 -d:00000433 -M time:unix:usec:utc -R 142 -R 40 -R 73 -R 113 -R 42 -M ID -M protocol -M level -C si -F "influx://${Your influx host}:8086/write?db=${your influx dbname}&p=${your influx password}TC&u=${your influx user}"
 ## I'll do this better in python with the .ini files soon.
