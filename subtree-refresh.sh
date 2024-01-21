6eb5ebc0a5ed5eede836e3a8c007f165567a0561
L. Green <leo@unixjerk.org>
2024-01-21 10:30:52 -0600

    Update subtree-refresh.sh

--- subtree-refresh.sh
+++ subtree-refresh.sh
@@ -1,2 +1,3 @@
 git subtree pull --prefix rtl_433 https://github.com/merbanan/rtl_433.git master --squash
 git subtree pull --prefix usbtenki https://github.com/Dracaltech/usbtenki.git master --squash
+git subtree pull --prefix pywws https://github.com/jim-easterbrook/pywws.git master --squash
