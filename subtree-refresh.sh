052a1ef60d9c7cf00aae26f38aaa9d978e424613
Leo Green <leo@unixjerk.org>
2024-01-21 10:05:41 -0600

    Subtrees are better

---
+++ subtree-refresh.sh
@@ -0,0 +1,2 @@
+git subtree pull --prefix rtl_433 https://github.com/merbanan/rtl_433.git master --squash
+git subtree pull --prefix usbtenki https://github.com/Dracaltech/usbtenki.git master --squash
