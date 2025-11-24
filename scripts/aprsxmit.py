a0e4146215beb38ef4322fab2c657415dbdaae88
Leo Green <leo@unixjerk.org>
2025-11-23 20:00:02 -0600

    Vibecoded fixes to zambretti forecasting module

--- scripts/aprsxmit.py
+++ scripts/aprsxmit.py
@@ -6,13 +6,13 @@ import aprslib
 import datetime
 import configparser
 import argparse
-import requests
-import re
-import math
-import time
+##import requests
+##import re
+##import math
+##import time
 import pytz
-import subprocess
-import sys
+#3import subprocess
+#import sys
 import metpy.calc as mpcalc
 from openweather_pws import Measurements
 from metpy.units import units
@@ -41,14 +41,19 @@ month=int(now.strftime('%m'))
 day=  int(now.strftime('%d'))
 thisMin = int(now.strftime('%M'))
 hourago = (datetime.datetime.now() - datetime.timedelta(hours=1))
+tenminago = (datetime.datetime.now() - datetime.timedelta(minutes=10))
 morning=datetime.datetime(year,month,day,0,0,0)
 morningTS= morning.timestamp()*1000
 morningTS=int(morningTS)
 morning=str(morningTS)
+
 houragoTS=hourago.timestamp()*1000
 houragoTS=int(houragoTS)
 hourago=str(houragoTS)
 
+tenminagoTS=tenminago.timestamp()*1000
+tenminagoTS=int(tenminagoTS)
+tenminago=str(tenminagoTS)
 
 ##Get config info
 config = configparser.ConfigParser(allow_no_value=True)
@@ -104,8 +109,6 @@ def fixInfluxOutput(input):
     output=str(input)
     if output == 'ResultSet({})':
        output = None
-       if DEBUG == True:
-           print ("fixInfluxOutput didn't find any output, writing a null instead")
     else:
        output=output.split(':')[5].split('}')[0]
        output=output.strip()
@@ -152,6 +155,14 @@ def getRainDiffQuery(measurement,delta):
        output=(max-min)
     return output
 
+def getAltimiterPressure(mbPressure,mHeight):
+   F1 = (1013.25 ** 0.190284) * 0.0065/288
+   F2 = (mHeight)/(((mbPressure) - 0.3) ** 0.190284)
+   F3 = 1/0.190284
+   F = (1 + ((F1) * (F2))) ** (F3)
+   alt = (mbPressure - 0.3) *F
+   return alt
+
 
 # Define influx queries to get information
 tempPint=(getQuery('temperature_C',thermometer,'int') * units.degC)
@@ -161,25 +172,63 @@ windDIRPint=(getWindDirQuery('wind_dir_deg',windStation,'mode','int') * units.de
 windGustPint=(getQuery('wind_max_km_h',windStation,'int') * units.kilometer_per_hour)
 rainDiffHourPint=(getRainDiffQuery(rainStation,hourago) * units.millimeter)
 rainDiffDayPint=(getRainDiffQuery(rainStation,morning) * units.millimeter)
+rainDiff10minPint=(getRainDiffQuery(rainStation,tenminago) * units.millimeter)
 lux=(getQuery('light_lux',windStation,'int'))
 uv=(getQuery('uvi',windStation,'int'))
-baroINHGPint=(getQuery('Barometer','Dracal',int) * units.inHg)
-baroMB=(baroINHGPint.to('hPa'))
-
-
+stationBaroINHGPint=(getQuery('UnCorrectedBarometer','Dracal',int) * units.inHg)
+stationBaroMB=(stationBaroINHGPint.to('hPa'))
 winddir=round(windDIRPint.magnitude)
 windspeed=round(windPint.to('mile_per_hour').magnitude)
 windgust=round(windGustPint.to('mile_per_hour').magnitude)
 temp=round(tempPint.to('degF').magnitude)
-rainHr=round(rainDiffHourPint.to('inch').magnitude)
-rain24Hr=round(rainDiffDayPint.to('inch').magnitude)
-baro=round(baroMB.magnitude *10)
+##rainHrPint=(rainDiffHourPint.to('inch').magnitude)
+rainHr=round(rainDiffHourPint.to('inch').magnitude,3)
+rainMidnight=round(rainDiffDayPint.to('inch').magnitude,3)
+rain10min=round(rainDiff10minPint.to('inch').magnitude,3)
+## Get altimiter from station pressure and altitide
+
+baroMB=getAltimiterPressure(stationBaroMB.magnitude,Altitude)
+print (baroMB)
+
+baro=round(baroMB*10)
 winddir=str(round(winddir)).zfill(3)
 windspeed=str(windspeed).zfill(3)
 windgust=str(windgust).zfill(3)
 temp=str(temp).zfill(3)
+solarRadiation=(lux/126.7)
+
+
+## Solar radiation is a little different, we need to make sure it's 3 digits
+
+if solarRadiation <= 999:
+   solarRadiationAPRSSymbol="L"
+   solarRadiationFormatted=str(int(solarRadiation)).zfill(3)
+   if DEBUG==True:
+      print ('solarRadiation is less than 999')
+elif solarRadiation >= 999:
+      solarRadiationFormatted=str(int(round(solarRadiation)/10)).zfill(3)
+      solarRadiationAPRSSymbol="l"
+      print ('solarRadiation is greater than 999')
+      
+
+if DEBUG==True:
+   print ('solarRadiation unmodified is: ',solarRadiation)
+   print ('solarRadiationAPRSSymbol is: ',solarRadiationAPRSSymbol)
+   print ('SolarRadiationFormatted is: ',solarRadiationFormatted)
+
+
+## Rain is a little different, we need to make sure it's 3 digits
+rainHr=int(rainHr*100)
+rainMidnight=int(rainMidnight*100)
+rain10min=int(rain10min*100)
+## then we need to stringify it with leading zeros if necessary
 rainHr=str(rainHr).zfill(3)
-rain24Hr=str(rain24Hr).zfill(3)
+rainMidnight=str(rainMidnight).zfill(3)
+rain10min=str(rain10min).zfill(3)
+
+if DEBUG == True:
+   print ('rainMidnight str is: ',rainMidnight)
+   print ('rainHr str is: ',rainHr)
 baro=str(baro)
 humidity=str(humidityPint.magnitude).zfill(2)
 
@@ -191,7 +240,20 @@ if DEBUG == True:
 AIS.connect()
 
 ## Format the weather message
-aprsWeatherFrame=('@'+(zuluday)+(zuluhour)+(zuluminute)+'z'+(aprsLat)+'/'+(aprsLong)+'_'+(winddir)+'/'+(windspeed)+'g'+(windgust)+'t'+(temp)+'r'+(rainHr)+'p'+(rain24Hr)+'P...'+'h'+(humidity)+'b'+(baro))
+aprsWeatherFrame=('@'+(zuluday)+\
+                     (zuluhour)+\
+                     (zuluminute)+\
+                     'z'+(aprsLat)+\
+                     '/'+(aprsLong)+\
+                     '_'+(winddir)+'/'+(windspeed)+\
+                     'g'+(windgust)+\
+                     't'+(temp)+\
+                     'r'+(rainHr)+\
+                     'P'+(rainMidnight)+\
+                     (solarRadiationAPRSSymbol)+(solarRadiationFormatted)\
+                     +'h'+(humidity)+\
+                     'b'+(baro)+\
+                     '#'+(rain10min))
 
 ## Format the APRS prefix
 aprsPrefix=((aprsCallsign)+'>ADPR16,WIDE1-1:')
